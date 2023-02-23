145481e2666c633633871a7f6f4b2cb8d05c9440
liuliumm <1753265357@qq.com>
2023-02-23 19:37:09 +0800

    API模块及请求封装

--- src/api/user.js
+++ src/api/user.js
@@ -4,8 +4,20 @@ export default {
   mutations: {},
   actions: {}
 }
+export function login(data) {
 
+}
+
+export function getInfo(token) {
+
+}
+
+export function logout() {
+
+}
 // import request from '@/utils/request'
+// 使用了封装的request工具，每个接口的请求都单独导出了一个方法，
+// 样做的好处就是，任何位置需要请求的话，可以直接引用我们导出的请求方法
 // export function login(data) {
 //   return request({
 //     url: '/vue-admin-template/user/login',
