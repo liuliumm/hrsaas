bc714e7105cad150bf88f3d19083f5c114742da9
liuliumm <102978510+liuliumm@users.noreply.github.com>
2023-02-23 20:10:42 +0800

    axios实例

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const service = axios.create() // 1.2创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
 service.interceptors.response.use() // 响应拦截器
-export default service // 导出axios实例
+export default service // 导出axios实例//
 
 // // 1、创建axios实例
 // // 1.1 引入axios
