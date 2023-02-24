e20308c4806e0b53ac097ac8a4d8dfa248265b6e
liuliumm <1753265357@qq.com>
2023-02-24 14:08:16 +0800

    将token值作为公共的访问属性放出

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,8 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device
-  // token: state => state.user.token,
+  device: state => state.app.device,
+  // 在根级的getters上 开发子模块的属性
+  token: state => state.user.token,  //为了更好的让其他模块和组件更好的获取token数据，将token值作为公共的访问属性放出
   // avatar: state => state.user.avatar,
   // name: state => state.user.name
 }
